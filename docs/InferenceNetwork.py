fc2b8d661870192f07ecdc69a511e189969c7f90
nishankjain <nishankjain@users.noreply.github.com>
2019-11-17 21:22:46 -0500

    Added docs

---
+++ docs/InferenceNetwork.py
@@ -0,0 +1,11 @@
+class InferenceNetwork:
+    def __init__(self, inverted_index):
+        """
+        Base class for initializing and inference network
+        """
+    
+    def get_operator(self, query_string, structured_query_operator, window_size):
+        """
+        Inititalizes an inference network with the given query string,
+        query operator and window size
+        """
